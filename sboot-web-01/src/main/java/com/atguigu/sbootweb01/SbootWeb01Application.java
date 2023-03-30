52380bdfcda3c515fb85606176bdbb41247ba0be
XuanLuomeng <1079716693@qq.com>
2023-03-30 16:40:23 +0800

    first

---
+++ sboot-web-01/src/main/java/com/atguigu/sbootweb01/SbootWeb01Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.sbootweb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootWeb01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootWeb01Application.class, args);
+    }
+
+}
