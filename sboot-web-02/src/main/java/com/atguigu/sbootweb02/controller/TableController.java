d83a6282250a4bac369295e57ad5115acab930b1
XuanLuomeng <1079716693@qq.com>
2023-04-18 23:21:14 +0800

    1.0.5

--- sboot-web-02/src/main/java/com/atguigu/sbootweb02/controller/TableController.java
+++ sboot-web-02/src/main/java/com/atguigu/sbootweb02/controller/TableController.java
@@ -1,6 +1,7 @@
 package com.atguigu.sbootweb02.controller;
 
 import com.atguigu.sbootweb02.bean.User;
+import com.atguigu.sbootweb02.exception.UserTooManyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,10 @@ public class TableController {
                 new User("haha", "aaaaaa"),
                 new User("hehe", "aaaddd"));
         model.addAttribute("users",users);
+
+//        if(users.size()>3){
+//            throw new UserTooManyException();
+//        }
         return "table/dynamic_table";
     }
 
